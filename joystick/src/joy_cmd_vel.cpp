e368ea6c6f6c0924be923406e470801516f60cad
Hong Jihyeon <hong091788@naver.com>
2023-11-23 09:41:14 +0900

    "third"
    wheel joystick code
    조이스틱이 위아래 움직였을때에만 바퀴가 동작함
    원상복귀 후 정지함

--- joystick/src/joy_cmd_vel.cpp
+++ joystick/src/joy_cmd_vel.cpp
@@ -4,8 +4,6 @@
 
 #define ANGULAR1_MAX 1.0  //각속도1의 최댓값 1.0 ~ -1.0
 #define ANGULAR2_MAX 1.0  //각속도2의 최댓값 1.0 ~ -1.0
-#define ACC_LINEAR 0.1
-#define ACC_ANGULAR 0.2
 
 class Joy_cmd_vel
 {
@@ -16,21 +14,19 @@ public:
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
-  ros::NodeHandle n;
+  ros::NodeHandle nh_;
 
-  float target_angular1;   //조이스틱에서 받은 목표 각속도1
-  float target_angular2;   //조이스틱에서 바은 목표 각속도2
-  float current_angular1;  //현재 각속도1 -> 값들이 목표 각속도1에 수렴하도록 함.
-  float current_angular2;  //현재 각속도2 -> 값들이 목표 각속도2에 수렴하도록 함.
+  float target_angular1;  //조이스틱에서 받은 목표 각속도1
+  float target_angular2;  //조이스틱에서 바은 목표 각속도2
 
   ros::Publisher vel_pub_;   // cmd_vel 토픽으로 로봇의 속도 명령을 발행.
   ros::Subscriber joy_sub_;  // joy 토픽에서 조이스틱 메시지를 받아 joyCallback 콜백 함수를 호출.
 };
 
-Joy_cmd_vel::Joy_cmd_vel() : target_angular1(0.0), target_angular2(0.0), current_angular1(0.0), current_angular2(0.0)
+Joy_cmd_vel::Joy_cmd_vel() : target_linear(0.0), target_angular(0.0)
 {
-  vel_pub_ = n.advertise<geometry_msgs::Twist>("/cmd_vel", 2);
-  joy_sub_ = n.subscribe<sensor_msgs::Joy>("joy", 10, &Joy_cmd_vel::joyCallback, this);
+  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &Joy_cmd_vel::joyCallback, this);
 }
 
 void Joy_cmd_vel::operate()
@@ -38,27 +34,27 @@ void Joy_cmd_vel::operate()
   geometry_msgs::Twist twist;
 
   // 현재 각속도1의 값이 1.0 ~ -1.0 사이에 있도록 함.
-  if (current_angular1 > ANGULAR1_MAX)
+  if (target_angular1 > ANGULAR1_MAX)
   {
-    current_angular1 = ANGULAR1_MAX;
+    target_angular1 = ANGULAR1_MAX;
   }
-  else if (current_angular1 < -ANGULAR1_MAX)
+  else if (target_angular1 < -ANGULAR1_MAX)
   {
-    current_angular1 = -ANGULAR1_MAX;
+    target_angular1 = -ANGULAR1_MAX;
   }
 
   // 현재 각속도2의 값이 1.0 ~ -1.0 사이에 있도록 함.
-  if (current_angular2 > ANGULAR2_MAX)
+  if (target_angular2 > ANGULAR2_MAX)
   {
-    current_angular2 = ANGULAR2_MAX;
+    target_angular2 = ANGULAR2_MAX;
   }
-  else if (current_angular2 < -ANGULAR2_MAX)
+  else if (target_angular2 < -ANGULAR2_MAX)
   {
-    current_angular2 = -ANGULAR2_MAX;
+    target_angular2 = -ANGULAR2_MAX;
   }
 
-  twist.linear.x = current_angular1;
-  twist.angular.z = current_angular2;
+  twist.linear.x = target_angular1;
+  twist.angular.z = target_angular2;
 
   vel_pub_.publish(twist);  //설정된 Twist 메시지를 발행함.
 }
@@ -66,7 +62,7 @@ void Joy_cmd_vel::operate()
 // 조이스틱 메시지에서 각속도 목표값을 읽어와 변수에 저장함.
 void Joy_cmd_vel::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  target_angular1 = joy->axes[3];  //조이스틱에서 오른쪽 방향키 위아래
+  target_angular1 = joy->axes[4];  //조이스틱에서 오른쪽 방향키 위아래
   target_angular2 = joy->axes[1];  //조이스틱에서 왼쪽 방향키 위아래
 }
 
